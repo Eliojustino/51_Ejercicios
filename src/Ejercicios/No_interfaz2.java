747c17bc01ba9257331e0851ae7ee826ed1089eb
ELIO <eliojustinoc@gmail.com>
2023-03-22 16:55:49 -0600

    Más ejercicios sin interfaz

---
+++ src/Ejercicios/No_interfaz2.java
@@ -0,0 +1,19 @@
+/**************************************
+ * Autor: Cruz Ortega Elio Justino    *
+ * Fecha Creación: 22/03/23           *
+ * Fecha Actualización: 22/03/23	  *
+ * Descripción: Ejercicios para		  * 
+ * 				viernes				  *
+ **************************************/
+
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class No_interfaz2 {
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		int a, b, c, d, e, f, g
+	}
+
+}
